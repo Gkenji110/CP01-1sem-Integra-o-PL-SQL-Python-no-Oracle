589b763d6f9e3779c46d0e1fb5881bea930500fd
Gustavo Kenji Terada <gustavo.terada01@gmail.com>
2026-03-11 15:29:41 -0300

    add index.py to run the app

---
+++ index.py
@@ -0,0 +1,101 @@
+from flask import Flask, request, render_template, redirect, url_for
+import oracledb
+import os
+
+app = Flask(__name__)
+
+def get_connection():
+    meu_dsn = oracledb.makedsn("oracle.fiap.com.br", 1521, service_name="orcl")
+
+    conn = oracledb.connect(
+        user="RM562745",
+        password="140101",
+        dsn=meu_dsn
+    )
+
+    return conn
+
+
+@app.route("/")
+def home():
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT id_ativo, nome, setor, preco_base, estoque
+        FROM tb_ativos_galacticos
+    """)
+
+    produtos = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return render_template("index.html", produtos=produtos)
+
+
+@app.route("/processar", methods=["POST"])
+def processar():
+
+    evento = request.form["evento"]
+    setor = request.form["setor"]
+    valor_str = request.form["valor"].strip().replace(",", ".")
+    valor = float(valor_str)
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    plsql = """
+    DECLARE
+
+        v_evento VARCHAR2(50) := :evento;
+        v_setor VARCHAR2(50) := :setor;
+        v_valor NUMBER := :valor;
+
+        v_novo_preco NUMBER;
+
+        CURSOR c_produtos IS
+            SELECT id_ativo, preco_base
+            FROM tb_ativos_galacticos
+            WHERE setor = v_setor;
+
+    BEGIN
+
+        FOR r IN c_produtos LOOP
+
+            v_novo_preco := r.preco_base;
+
+            IF v_evento = 'RADIACAO' THEN
+                v_novo_preco := r.preco_base * (1 + v_valor/100);
+
+            ELSIF v_evento = 'CRISE' THEN
+                v_novo_preco := r.preco_base * (1 - v_valor/100);
+            END IF;
+
+            UPDATE tb_ativos_galacticos
+            SET preco_base = v_novo_preco
+            WHERE id_ativo = r.id_ativo;
+
+        END LOOP;
+
+        COMMIT;
+
+    END;
+    """
+
+    cursor.execute(plsql, {
+        "evento": evento,
+        "setor": setor,
+        "valor": valor
+    })
+
+    cursor.close()
+    conn.close()
+
+    return redirect(url_for('home'))
+
+    return render_template("index.html", produtos=produtos)
+
+if __name__ == "__main__":
+    app.run(debug=True)
